57b613639412552004eaa401554b5ad0b077b31b
tanishkulshrestha <tanish.kulshrestha@successive.tech>
2019-01-30 15:51:06 +0530

    Refactor: modify code

--- src/controllers/trainee/routes.ts
+++ src/controllers/trainee/routes.ts
@@ -8,13 +8,8 @@ import { TRAINEEE } from "../../libs/constants";
 
 const traineeRoute: express.Router = express.Router();
 console.log("Inside routes.ts");
-traineeRoute.get("/", authMiddleWare(TRAINEEE, "write"), trainee.get);
-// traineeRoute.get("/", validationHandler(validation.get), trainee.get);
-traineeRoute.post("/", validationHandler(validation.create), trainee.create);
-traineeRoute.put("/", validationHandler(validation.update), trainee.update);
-traineeRoute.delete(
-  "/:id",
-  validationHandler(validation.delete),
-  trainee.remove
-);
+traineeRoute.get( "/" , authMiddleWare(TRAINEEE, "read") /*, validationHandler(validation.get) */, trainee.get );
+traineeRoute.post( "/" , authMiddleWare(TRAINEEE, "write") /*, validationHandler(validation.create) */, trainee.create );
+traineeRoute.put( "/" , authMiddleWare(TRAINEEE, "update") /*, validationHandler(validation.update) */, trainee.update );
+traineeRoute.delete( "/:id" , authMiddleWare(TRAINEEE, "delete")/* , validationHandler(validation.delete)*/ , trainee.remove );
 export default traineeRoute;
